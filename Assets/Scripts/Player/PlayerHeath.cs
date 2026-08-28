324ba0155fc336c9d1d3f5e2c7ec63dea10b9f0b
denzmov <denzmov@gmail.com>
2026-08-28 12:01:46 +0200

    + Player Heath

---
+++ Assets/Scripts/Player/PlayerHeath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using GameCore.Health;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerHeath : ObjectHealth
+    {
+        private WaitForSeconds _regenerationInterval = new WaitForSeconds(5f);
+        private float _regenerationValue = 1f;
+
+        private void Start() => StartCoroutine(Regeration());
+
+        public override void TakeDamage(float damage)
+        {
+            base.TakeDamage(damage);
+            if(CurrentHealth <= 0)
+                Debug.Log("Player is dead");
+        }
+        private IEnumerator Regeration()
+        {
+            while (true)
+            {
+                TakeHeal(_regenerationValue);
+                yield return _regenerationInterval;
+            }
+        }
+        
+    }
+    
+}
