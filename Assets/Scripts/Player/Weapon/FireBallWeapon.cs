5902f43bda16aaee6b67429fb69de15e99d293cb
denzmov <denzmov@gmail.com>
2026-09-03 14:03:28 +0200

    + FireBall x3

--- Assets/Scripts/Player/Weapon/FireBallWeapon.cs
+++ Assets/Scripts/Player/Weapon/FireBallWeapon.cs
@@ -19,6 +19,12 @@ namespace Player.Weapon
         [SerializeField] private List<Transform> _transformSprite2X;
         [SerializeField] private Transform _targetCaontainer2X;
         
+        [Header("Triple")]
+        [SerializeField] private List<SpriteRenderer> _spriteRenderer3X = new List<SpriteRenderer>();
+        [SerializeField] private List<Collider2D> _collider3X;
+        [SerializeField] private List<Transform> _transformSprite3X;
+        [SerializeField] private Transform _targetCaontainer3X;
+        
         
         private WaitForSeconds _interval, _duration, _timeBetweenAttack;
         private float _rotationSpeed, _range;
@@ -29,11 +35,13 @@ namespace Player.Weapon
             SetStats(0);
             SetupWeapon();
             Activate();
-            // LevelUp();
-            // LevelUp();
-            // LevelUp();
-            // LevelUp();
-            // LevelUp();
+            LevelUp();
+            LevelUp();
+            LevelUp();
+            LevelUp();
+            LevelUp();
+            LevelUp();
+            LevelUp();
         }
 
         private void Update() => transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime);
@@ -63,23 +71,39 @@ namespace Player.Weapon
 
         private void SetupWeapon()
         {
-            if (CurrentLevel < 4)
+            if (CurrentLevel < 4) // одиночный шар до 4 уровня
             {
                 _targetCaontainer1X.gameObject.SetActive(true);
                 _targetCaontainer2X.gameObject.SetActive(false);
+                _targetCaontainer3X.gameObject.SetActive(false);
                 _transformSprite1X.localPosition = new Vector3(_range, 0, 0);
                 _collider1X.offset = new Vector2(_range, 0);
             }
-            else
+            else if (CurrentLevel < 6) // два шара на уровнях 4-5
             {
                 _targetCaontainer1X.gameObject.SetActive(false);
                 _targetCaontainer2X.gameObject.SetActive(true);
+                _targetCaontainer3X.gameObject.SetActive(false);
                 for (int i = 0; i < _collider2X.Count; i++)
                     _collider2X[i].gameObject.SetActive(true);
                 _transformSprite2X[0].localPosition = new Vector3(_range, 0, 0);
                 _transformSprite2X[1].localPosition = new Vector3(-_range, 0, 0);
                 _collider2X[0].offset = new Vector2(_range, 0);
                 _collider2X[1].offset = new Vector2(-_range, 0);
+            }
+            else // три шара на уровнях 6+
+            {
+                _targetCaontainer1X.gameObject.SetActive(false);
+                _targetCaontainer2X.gameObject.SetActive(false);
+                _targetCaontainer3X.gameObject.SetActive(true);
+                for (int i = 0; i < _collider3X.Count; i++)
+                    _collider3X[i].gameObject.SetActive(true);
+                _transformSprite3X[0].localPosition = new Vector3(_range, 0, 0);
+                _transformSprite3X[1].localPosition = new Vector3(_range, 0, 0);
+                _transformSprite3X[2].localPosition = new Vector3(-_range, 0, 0);
+                _collider3X[0].offset = new Vector2(_range, 0);
+                _collider3X[1].offset = new Vector2(-_range, 0);
+                _collider3X[2].offset = new Vector2(-_range, 0);
             }
         }
         
@@ -92,7 +116,7 @@ namespace Player.Weapon
                     _spriteRenderer1X.enabled = !_spriteRenderer1X.enabled;
                     _collider1X.enabled = !_collider1X.enabled;
                 }
-                else
+                else if (CurrentLevel < 6)
                 {
                     for (int i = 0; i < _spriteRenderer2X.Count; i++)
                     {
@@ -100,7 +124,16 @@ namespace Player.Weapon
                         _collider2X[i].enabled = !_collider2X[i].enabled;
                     }
                 }
-                _interval = _spriteRenderer1X.enabled || _spriteRenderer2X[0] ? _duration : _timeBetweenAttack;
+                else
+                {
+                    for (int i = 0; i < _spriteRenderer3X.Count; i++)
+                    {
+                        _spriteRenderer3X[i].enabled = !_spriteRenderer3X[i].enabled;
+                        _collider3X[i].enabled = !_collider3X[i].enabled;
+                    }
+                }
+                
+                _interval = _spriteRenderer1X.enabled || _spriteRenderer2X[0] || _spriteRenderer3X[0] ? _duration : _timeBetweenAttack;
                 yield return _interval;
             }
         }
